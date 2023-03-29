118b8e649be194da33525aaff9c159826026beb5
vip690 <vip.orlova69@mail.ru>
2023-03-29 09:47:16 +0400

    Просто тестовый исходник

--- Unit1vij.pas
+++ Unit1vij.pas
@@ -17,16 +17,12 @@ type
     { Public declarations }
   end;
 
-
-//const w='ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏĞÑÒÓÔÕÖ×ØÙÚÛÜİŞß_';
-const w='ABCDEFGHIJKLMNOPQRSTUVWXYZÄÜÖß';
-//e='ØÂÌÁÓÆÍß';
-//e='ßÁÕÚØŞÌÕ';
-e='CXLJWWO` ÖNXÄHVVOHNQßCH AJB XTW ÖÜVÜXBÜßLÖXU UNWAPWJIHQW WB MJL KRV VSVSJEÜVKQSC ÖNXÄHVVOHNQßCHV';
+const
+    w='ABCDEFGHIJKLMNOPQRSTUVWXYZÄÜÖß';
+		e='CXLJWWO` ÖNXÄHVVOHNQßCH AJB XTW ÖÜVÜXBÜßLÖXU UNWAPWJIHQW WB MJL KRV VSVSJEÜVKQSC ÖNXÄHVVOHNQßCHV';
 var
-  Form1: TForm1;
-  //key: string='ÎĞÅÕ';
-  key: string='USCHINSKI'; //
+	Form1: TForm1;
+	key: string='USCHINSKI';
 
 
 implementation
@@ -34,30 +30,35 @@ implementation
 {$R *.dfm}
 
 procedure TForm1.FormCreate(Sender: TObject);
-var i,j,k,g,i2: integer; c: char; s,u: string;
-a2:integer;
+var i,j,k,g,i2: integer; 
+	c: char; 
+	s,u: string;
+	a2:integer;
 begin
-s:=''; u:='';
-i2:=1;
-for i:=1 to length(e) do
-if pos(e[i],w)>0 then
-	begin
-		g:=1+((i2-1)mod length(key));//
-		c:=key[g];
-		s:=s+c;  // êëş÷
-		k:=pos(c,w)+1;
-		j:=pos(e[i],w);
-		a2:=length(w);
-		u:=u+w[1+((j-1)-(k-1)+a2*2)mod a2];
-    inc(i2);
-	end
-	else
-	begin
-    s:=s+e[i];
-		u:=u+e[i]
-	end;
-memo1.Lines.Add(e);memo1.Lines.Add(s);    memo1.Lines.Add(u);
-memo2.Text:=u;
+	s:='';
+	u:='';
+	i2:=1;
+	for i:=1 to length(e) do
+	if pos(e[i],w)>0 then
+		begin
+			g:=1+((i2-1)mod length(key));
+			c:=key[g];
+			s:=s+c;  // êëş÷
+			k:=pos(c,w)+1;
+			j:=pos(e[i],w);
+			a2:=length(w);
+			u:=u+w[1+((j-1)-(k-1)+a2*2)mod a2];
+			inc(i2);
+		end
+		else
+		begin
+			s:=s+e[i];
+			u:=u+e[i]
+		end;
+	memo1.Lines.Add(e);
+	memo1.Lines.Add(s);
+	memo1.Lines.Add(u);
+	memo2.Text:=u;
 end;
 {ÂÛ×ÈÑËÈÒÅËÜÍÀß ÌÀØÈÍÀ ËÅÉÁÍÈÖÀ ÑÒÀËÀ ÈÑÒÎĞÈ×ÅÑÊÎÉ ÂÅÕÎÉ Â ÑÎÇÄÀÍÈÈ ÌÅÕÀÍÈ×ÅÑÊÈÕ ÂÛ×ÈÑËÈÒÅËÜÍÛÕ ÌÀØÈÍ}
 end.
